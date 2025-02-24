ef239f55fab9dd75beaa0167b023b53899562d8c
Vadim Melnikov <vladmlnkv@icloud.com>
2025-02-24 13:48:54 +0200

    finished

--- burger-menu.js
+++ burger-menu.js
@@ -1,5 +1,7 @@
-const burgerMenu = document.getElementById("burger");
+var burgerMenu = document.getElementById("burger");
 const menu = document.getElementById("burger-menu");
+const menuLinks = document.querySelectorAll("#burger-menu li");
+
 
 burgerMenu.addEventListener("click", () => {
   if (menu.classList.contains("active")) {
@@ -9,4 +11,12 @@ burgerMenu.addEventListener("click", () => {
     menu.classList.add("active");
     burgerMenu.src = "assets/X.svg";
   }
+});
+
+
+menuLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    burgerMenu.src = "assets/burger.svg";
+    menu.classList.remove("active");
+  });
 });
